6bb5c4a2afcf37694a1e7458061a39d2763ffcf7
Salah Cherkaoui <salah.cherkaoui@caseware.com>
2019-07-22 17:04:44 -0400

    updates

--- getComputerName.ps1
+++ getComputerName.ps1
@@ -3,15 +3,27 @@
 #script takes OU name as argument if not supplied then default is WebApps
 
 
-$ou= "WebApps"
-$Ffile = ".\Downloads\listServers.txt"
+
+ param (
+    [string]$ou = "WebApps",
+    [string]$domainName = $( Read-Host "Input domain, please" )
+    #[switch]$force = $false
+ )
+
+
+#$ou= "WebApps"
+$domain = "DC=$($domainName),DC=com"
+
+Write-Host "this is the domain " $domain
+
+$Ffile = "~\Downloads\listServers.txt"
 Clear-Content $Ffile
 
 #$listOU = Get-ADOrganizationalUnit -Filter 'Name -like $ou' | Select-Object -ExpandProperty DistinguishedName | Out-File $Sfile
 
-$listOU = Get-ADOrganizationalUnit -Filter 'Name -like $ou' | Select-Object -ExpandProperty DistinguishedName
+$listOU = Get-ADOrganizationalUnit -Filter 'Name -like $ou' -SearchBase $domain | Select-Object -ExpandProperty DistinguishedName
 
-#Write-Host "list of OU :" $listOU
+Write-Host "list of OU :" $listOU
 #Write-Host "searching for computers under all previous OU"
 
 foreach ($var in $listOU){
